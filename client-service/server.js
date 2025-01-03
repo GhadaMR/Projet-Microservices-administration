2c5d5a0596c00811b719f52cf31a4e249e1c4e2d
GhadaMR <ghadamrassi9@gmail.com>
2025-01-03 18:59:42 +0100

    corr dapr

--- client-service/server.js
+++ client-service/server.js
@@ -11,17 +11,17 @@ app.use(bodyParser.json()); // Pour analyser le corps des requêtes en JSON
 
 // Connexion à MongoDB
 mongoose.connect('mongodb://admin:password@localhost:27017/client_db?authSource=admin', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Failed to connect to MongoDB', err));
 
-  // Routes
+// Routes
 app.use('/api/clients', clientRoutes);
 
 // Lancer le serveur
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Client Service running on port ${PORT}`);
 });
